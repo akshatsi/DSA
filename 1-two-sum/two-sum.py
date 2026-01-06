d97489db861c626b0eb72a8056f28682bed19d89
akshatsi <84762984+akshatsi@users.noreply.github.com>
2026-01-06 17:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (47.03%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,9 +5,13 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            x = 0
-            for j in range(i+1, len(nums)):
-                x = nums[i] + nums[j]
-                if x == target:
-                    return [i,j]
+        numbers_seen = {}
+
+        for index in range(len(nums)):
+            current = nums[index]
+            needed = target - current
+
+            if needed in numbers_seen:
+                return [numbers_seen[needed], index]
+
+            numbers_seen[current] = index
