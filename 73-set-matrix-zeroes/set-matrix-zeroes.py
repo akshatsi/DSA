b2ec0d84b85b250745b2d17541b269fca8983b30
akshatsi <84762984+akshatsi@users.noreply.github.com>
2026-01-09 21:56:29 +0530

    Time: 8 ms (28.74%) | Memory: 13 MB (76.33%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
-        for i in range(len(matrix)):
+        '''for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
                     for col in range(len(matrix[0])):
@@ -17,8 +17,25 @@ class Solution(object):
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == -298964:
-                    matrix[i][j] = 0
+                    matrix[i][j] = 0'''
+        '''better'''
+        m = len(matrix)
+        n = len(matrix[0])
+        row = [0] * m
+        col= [0] * n
+        for i in range(m):
 
+            for j in range(n):
+
+                if matrix[i][j] == 0:
+                    row[i] = 1
+                    col[j] = 1
+
+        for i in range(m):
+            for j in range(n):
+                if row[i] == 1 or col[j] == 1:
+                    matrix[i][j] = 0
+                
 
 
         
