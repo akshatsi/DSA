7d4493ad67b2d371bde4c0f2afb34bf2437210a9
akshatsi <84762984+akshatsi@users.noreply.github.com>
2026-01-13 01:12:59 +0530

    Time: 15 ms (38.56%) | Memory: 18.1 MB (12.71%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,5 +1,25 @@
 class Solution(object):
     def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        '''a = len(nums)
+        freq = {}
+        for i in range (a):
+            current = nums[i]
+            if current in freq:
+                freq[current]+= 1
+                continue 
+            freq[current] = 1   
+        values = freq.values()
+        result = []
+        for i in values:
+            if i > a/3:
+                for key, value in freq.items():
+                    if value == i:
+                        result.append(key)
+        return result'''
         a = len(nums)
         freq = {}
         result = []
@@ -12,3 +32,5 @@ class Solution(object):
                 result.append(key)
 
         return result
+                
+        
