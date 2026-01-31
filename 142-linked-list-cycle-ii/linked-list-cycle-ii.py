2afb457348494a5f8903a1de5869214681a3524f
akshatsi <84762984+akshatsi@users.noreply.github.com>
2026-01-31 23:11:27 +0530

    Time: 48 ms (58.77%) | Memory: 22.3 MB (32.61%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -6,7 +6,7 @@
 
 class Solution:
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp = head
+        '''temp = head
         arr = []
         while temp:
             if temp in arr:
@@ -14,4 +14,17 @@ class Solution:
             else:
                 arr.append(temp)
             temp = temp.next
+        return None'''
+        slow = head
+        fast = head
+        while fast and fast.next and slow:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                slow = head
+                while slow != fast:
+                    slow = slow.next
+                    fast = fast.next
+
+                return slow
         return None
