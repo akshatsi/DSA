9f4e33b36c85da4423982bb023aecd79fec046bc
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-05-26 11:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 21.2 MB (44.81%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        fin_dic = {}
+        '''fin_dic = {}
         for i in range(len(nums)):
             fin_dic[nums[i]] = fin_dic.get(nums[i], 0) + 1
 
-        return min(fin_dic, key=fin_dic.get)
+        return min(fin_dic, key=fin_dic.get)'''
+
+        counter = 0
+        for i in range(len(nums)):
+            counter ^= nums[i]
+
+        return counter
