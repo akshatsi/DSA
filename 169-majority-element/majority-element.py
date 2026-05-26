92f211f50f694a220867ede927424da7277d68a9
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-05-26 17:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (87.70%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        fin_dic = {}
+        '''fin_dic = {}
         for i in range(len(nums)):
             fin_dic[nums[i]] = fin_dic.get(nums[i], 0) + 1
 
-        return max(fin_dic, key = fin_dic.get)
+        return max(fin_dic, key = fin_dic.get)'''
+
+        nums.sort()
+        return nums[len(nums)//2]
