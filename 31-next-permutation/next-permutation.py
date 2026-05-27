516671d151c1bc75006c5aabfb25ca968b07b0a8
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-05-27 12:53:51 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (66.14%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -1,19 +1,20 @@
-class Solution(object):
-    def nextPermutation(self, nums):
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
         fixed = -1
-
-        for i in range(len(nums) - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
+        for i in range(len(nums) -2 , -1, -1):
+            if nums[i] < nums[i+1]:
                 fixed = i
                 break
-
         if fixed == -1:
             nums.reverse()
             return
 
-        for i in range(len(nums) - 1, fixed, -1):
+        for i in range(len(nums)-1, fixed, -1 ):
             if nums[i] > nums[fixed]:
                 nums[i], nums[fixed] = nums[fixed], nums[i]
                 break
 
-        nums[fixed + 1:] = reversed(nums[fixed + 1:])
+        nums[fixed+1:] = reversed(nums[fixed+1:])
