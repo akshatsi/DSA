2f7eac4fb1c11aa752a5b5c8807b453706914a82
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-05-28 16:19:16 +0530

    Time: 8 ms (48.18%) | Memory: 23.3 MB (35.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,36 +1,15 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        '''a = len(nums)
-        freq = {}
-        for i in range (a):
-            current = nums[i]
-            if current in freq:
-                freq[current]+= 1
-                continue 
-            freq[current] = 1   
-        values = freq.values()
-        result = []
-        for i in values:
-            if i > a/3:
-                for key, value in freq.items():
-                    if value == i:
-                        result.append(key)
-        return result'''
-        a = len(nums)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
         freq = {}
         result = []
 
         for num in nums:
-            freq[num] = freq.get(num, 0) + 1
+            freq[num] = freq.get(num,0) +1
 
         for key, value in freq.items():
-            if value > a / 3:
+            if value > n/3:
                 result.append(key)
 
         return result
-                
         
