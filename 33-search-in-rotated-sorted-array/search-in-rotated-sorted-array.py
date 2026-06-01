04a387999859a9e2e6e89bca1e9be5ff0da714f0
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-06-01 15:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (39.62%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -4,14 +4,17 @@ class Solution:
         high = len(nums) - 1
 
         while low <= high:
-            mid = low + (high - low) // 2
+            mid = (low + high)// 2
             if nums[mid] == target:
                 return mid
-            if nums[low] <= nums[mid]:
+
+            if nums[mid] >= nums[low]:
                 if nums[low] <= target < nums[mid]:
                     high = mid - 1
+
                 else:
                     low = mid + 1
+
             else:
                 if nums[mid] < target <= nums[high]:
                     low = mid + 1
