af0bb2c2ec997fadbe1ddb17c5a771f11580304b
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-06-09 23:21:24 +0530

    Time: 35 ms (56.31%) | Memory: 42.4 MB (86.89%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,36 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def reverseList(self, head):
-        prev = None
-        curr = head
-        while curr:
-            nxt = curr.next
-            curr.next = prev
-            prev = curr
-            curr = nxt
-        return prev
-
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        def reverse_list(head):
+            temp = head
+            prev = None
 
-        if not head or not head.next:
-            return True
+            while temp:
+                front = temp.next
+                temp.next = prev
+                prev = temp 
+                temp = front
+            return prev
 
-        # Step 1: find middle
-        slow = fast = head
+        
+        slow = head
+        fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
 
-        # Step 2: reverse second half
-        second_half = self.reverseList(slow)
-
-        # Step 3: compare halves
         first = head
-        second = second_half
+        second = reverse_list(slow)
 
         while second:
             if first.val != second.val:
                 return False
+
             first = first.next
             second = second.next
 
         return True
+        
