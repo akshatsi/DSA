0f699a7ae47831ddb8781b20d81423c548820251
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-06-15 12:46:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.82%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,24 +1,24 @@
 class Solution:
     def nextGreaterElement(self, nums1, nums2):
         result = [0] * len(nums1)
-        next_greater = [0] * len(nums2)
+        nextGreater = [0] * len(nums2)
         stack = []
-
-        for i in range(len(nums2) - 1, -1, -1):
+        for i in range(len(nums2) -1, -1,-1):
             while stack and nums2[i] >= stack[-1]:
                 stack.pop()
+            
             if not stack:
-                next_greater[i] = -1
+                nextGreater[i] = -1
+
             else:
-                next_greater[i] = stack[-1]
+                nextGreater[i] = stack[-1]
+
             stack.append(nums2[i])
 
-        # Step 2: Map nums2 elements to their next greater
         map_ = {}
-        for i in range(len(nums2)):
-            map_[nums2[i]] = next_greater[i]
+        for i in range (len(nums2)):
+            map_[nums2[i]] = nextGreater[i]
 
-        # Step 3: Build result using nums1
         for i in range(len(nums1)):
             result[i] = map_[nums1[i]]
 
