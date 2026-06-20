21614d8ca851cd71f0a14ebd8d413125255f7c74
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-06-20 12:34:43 +0530

    Time: 99 ms (64.39%) | Memory: 19.3 MB (85.70%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.py
@@ -1,15 +1,15 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         freq = [0,0,0]
-        l = 0
-        r = 0
+        low = 0
+        high = 0
         res = 0
-        for r in range(len(s)):
-            freq[ord(s[r])- ord('a')] += 1 #converts to ascii easy to map without dictionary
-            while  freq[0] > 0 and freq[1] > 0 and freq[2] > 0:
-                n = len(s) - r
+        for high in range(len(s)):
+            freq[ord(s[high]) - ord('a')] += 1
+            while freq[0] > 0 and freq[1] > 0 and freq[2] > 0:
+                n = len(s) - high
                 res += n
-                freq[ord(s[l])- ord('a')] -= 1
-                l+= 1
+                freq[ord(s[low])-ord('a')] -= 1
+                low += 1
 
         return res
