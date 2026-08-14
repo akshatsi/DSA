47f669c4a3dee6fb1df103bfda274c2e93e8c8ad
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-08-14 18:48:24 +0530

    Time: 3 ms (79.72%) | Memory: 20.5 MB (75.73%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.py
@@ -7,7 +7,7 @@ class Solution:
             total = numbers[left] + numbers[right]
             if total == target:
                 return [left + 1, right + 1]
-            elif total < target:
+            if total < target:
                 left += 1
             else:
                 right -= 1
