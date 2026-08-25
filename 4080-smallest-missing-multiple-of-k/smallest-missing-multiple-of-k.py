0739d15a922bff1eca5de602d147a437545ac07d
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-08-25 14:36:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (18.61%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingMultiple(self, nums: List[int], k: int) -> int:
-        res = float('inf')
+        '''res = float('inf')
         if k not in nums:
             return k
         for i in range(0,len(nums)):
@@ -10,6 +10,13 @@ class Solution:
         while res in nums:
             if res + k not in nums:
                 return res + k
-            res+= k
+            res+= k '''
             
 
+        num = set(nums)
+        i = 1
+        while True:
+            if i * k not in num:
+                return i *k
+            else:
+                i += 1
