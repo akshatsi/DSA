b6e299defdf5ba8d10b1b09a8e800a1c04ebd4bd
Akshat Singh <84762984+akshatsi@users.noreply.github.com>
2026-08-26 22:07:14 +0530

    Time: 2714 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,7 @@
-SELECT EmployeeUNI.unique_id, Employees.name
-FROM Employees
-LEFT JOIN EmployeeUNI
-ON Employees.id = EmployeeUNI.id;
+
+
+select eu.unique_id, e.name from 
+    Employees e 
+    left join EmployeeUNI eu
+    on
+    e.id = eu.id;
